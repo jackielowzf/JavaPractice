bb4f5f41c1ab3e57fcee1054590f7c95cb2347ab
jackielowzf <low_zhenfeng@yahoo.com>
2025-04-30 08:38:08 -0400

    Started chapter 5.26

---
+++ src/Chapter5/Chapter5_26.java
@@ -0,0 +1,21 @@
+package Chapter5;
+/*
+ * Palindromic prime
+ * A palindromic prime is a prime number and also palindromic. For example, 131 is a prime and also a palindromic prime, as are 313 
+ * and 757. Write a program that displays the first 100 palindromic prime numbers. Display 10 numbers per line, 
+ * separated by exactly one space, as follows (in textbook):
+ */
+public class Chapter5_26 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void isPrime() {
+		
+	}
+	
+	public static void isPalindrome() {
+		
+	}
+
+}
