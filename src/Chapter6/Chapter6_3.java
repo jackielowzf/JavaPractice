bdb981fd6df4afa3a5193675b5004020c6937322
jackielowzf <low_zhenfeng@yahoo.com>
2025-05-21 00:31:46 -0400

    Started Chpater 6.3

---
+++ src/Chapter6/Chapter6_3.java
@@ -0,0 +1,16 @@
+package Chapter6;
+/*
+ * Count occurrence of numbers
+ * Write a program that reads the integers between 1 and 100 and counts the
+ * occurrences of each. Assume the input ends with 0. Here is a sample run (in textbook)
+ * 
+ * Note that if a number occurs more than one time, the plural word times is used in the output.
+ */
+import java.util.Scanner;
+
+public class Chapter6_3 {
+	public static void main(String[] args) {
+		
+	}
+
+}
