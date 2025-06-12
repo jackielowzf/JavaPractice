31a25bacc9cf27faaf1eca754808bbca99ac83d9
jackielowzf <low_zhenfeng@yahoo.com>
2025-06-12 17:28:38 -0400

    Chapter 6.3

--- src/Chapter6/Chapter6_3.java
+++ src/Chapter6/Chapter6_3.java
@@ -10,6 +10,29 @@ import java.util.Scanner;
 
 public class Chapter6_3 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int[] numbers = new int[100];
+		int i = 0;
+				
+		System.out.println("Enter the integers between 1 and 100: ");
+		do {
+			i = input.nextInt();
+			if (i >= 1 && i <= 100) {
+				numbers[i - 1]++;
+			}
+		}while (i != 0);
+		
+		input.close();
+		
+		for (int j = 0; j < numbers.length; j++) {
+			if (numbers[j] != 0) {
+				if (numbers[j] == 1)
+				System.out.println((j + 1) + " occurs " + numbers[j] + " time");
+				else
+				System.out.println((j + 1) + " occurs " + numbers[j] + " times");
+			}
+		}
 		
 	}
 
