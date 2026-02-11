b6db7f3a28381cd87e3e994c3a2352f0a0ccee10
unknown <jackielowzhenfeng@yahoo.com>
2026-02-10 19:18:14 -0500

    Chapter6.22

--- src/Chapter6/Chapter6_22.java
+++ src/Chapter6/Chapter6_22.java
@@ -9,13 +9,71 @@ import java.util.Scanner;
  * solution. A sample output is shown below: (according to textbook)
  */
 public class Chapter6_22 {
-	public static void main(String args[]) {
-		
-		int[][] chessboard = new int[8][8];
-		
-		int i = 0;
+	 static final int N = 8;
 
-}
+	    public static void main(String[] args) {
+	        int[][] board = new int[N][N];
+
+	        if (solve(board, 0)) {
+	            printBoard(board);
+	        } else {
+	            System.out.println("No solution exists");
+	        }
+	    }
+
+	    // Try to place queens starting from column col
+	    static boolean solve(int[][] board, int col) {
+	        if (col == N) {
+	            return true; // all queens placed
+	        }
+
+	        for (int row = 0; row < N; row++) {
+	            if (isSafe(board, row, col)) {
+	                board[row][col] = 1;
+
+	                if (solve(board, col + 1)) {
+	                    return true;
+	                }
+
+	                // backtrack
+	                board[row][col] = 0;
+	            }
+	        }
+	        return false;
+	    }
+
+	    // Check if a queen can be placed at board[row][col]
+	    static boolean isSafe(int[][] board, int row, int col) {
+
+	        // Check left side of row
+	        for (int i = 0; i < col; i++) {
+	            if (board[row][i] == 1)
+	                return false;
+	        }
+
+	        // Check upper-left diagonal
+	        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+	            if (board[i][j] == 1)
+	                return false;
+	        }
+
+	        // Check lower-left diagonal
+	        for (int i = row, j = col; i < N && j >= 0; i++, j--) {
+	            if (board[i][j] == 1)
+	                return false;
+	        }
+
+	        return true;
+	    }
+
+	    static void printBoard(int[][] board) {
+	        for (int i = 0; i < N; i++) {
+	            for (int j = 0; j < N; j++) {
+	                System.out.print(board[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
 	
 	
 }
