3748f9f13b1278f40da5052f5b8bbfdf193342c8
unknown <jackielowzhenfeng@yahoo.com>
2026-03-22 22:28:22 -0400

    Chapter7.8

--- src/Chapter7/Chapter7_8.java
+++ src/Chapter7/Chapter7_8.java
@@ -15,7 +15,40 @@ public class Chapter7_8 {
 							 {4, -0.5}};
 		
 		int p1 = 0, p2 =1;
+		double shortestDistance = distance(points[p1][0], points[p1][1],
+				points[p2][0], points[p2][1]);
 		
+		for (int i = 0; i < points.length; i++) {
+			for (int j = i + 1; j < points.length; j++) {
+				double distance = distance(points[i][0], points[i][1],
+						points[j][0], points[j][1]);
+				
+				if (shortestDistance > distance) {
+					p1 = i;
+					p2 = j;
+					shortestDistance = distance;
+				}
+			}
+		}
+		
+		System.out.println("The closest two points are: ");
+		
+		for (int i = 0; i < points.length; i++) {
+			for (int j = i + 1; j < points.length; j++) {
+				double distance = distance(points[i][0], points[i][1],
+						points[j][0], points[j][1]);
+				
+				if (shortestDistance == distance) {
+					System.out.println("(" + points[i][0] + ", " + points[i][1] + ") and (" +
+							points[j][0] + ", " + points[j][1] + ")");
+					
+					
+				}
+			}
+		}
+		
+		
+				
 	}
 	
 	public static double distance(double x1, double y1, double x2, double y2) {
