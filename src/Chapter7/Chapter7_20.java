49bf27b0552f87a3f709b370a598f52e4eb6f0a9
jackielowzf <low_zhenfeng@yahoo.com>
2026-08-05 01:11:00 -0400

    Update on Chapter 7.20

---
+++ src/Chapter7/Chapter7_20.java
@@ -0,0 +1,18 @@
+package Chapter7;
+/*
+ * Chapter 7.20
+ * (Game: Connect four) Connect four is a two-player board game in which the players alternately drop 
+ * colored disks into a seven-column, six-row vertically suspended grid, as shown below.
+ * 
+ * The objective of the game is to connect four same-colored disks in a row, a column, or diagonal before
+ * your opponent can do likewise. The program prompts two players to drop a red or yellow disk alternately.
+ * In the preceding figure, the red disk is shown in a dark color and the yellow in a light color. 
+ * Whenever a disk is dropped, the program redisplays the board on the console and determines the status of 
+ * the game (win, draw, or continue).
+ */
+public class Chapter7_20 {
+	public static void main(String[] args) {
+		
+	}
+
+}
