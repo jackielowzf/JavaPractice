8b5a7ffeae649bec1c7ec9010f4a6163de5ad14e
jackielowzf <low_zhenfeng@yahoo.com>
2026-08-20 11:07:42 -0400

    Chapter 6.22

--- src/Chapter6/Chapter6_22.java
+++ src/Chapter6/Chapter6_22.java
@@ -1,6 +1,4 @@
 package Chapter6;
-import java.util.Scanner;
-
 /*
  * Game: Eight Queens
  * The classic Eight Queens puzzle is to place eight queens on a chessboard such that 
@@ -9,13 +7,58 @@ import java.util.Scanner;
  * solution. A sample output is shown below: (according to textbook)
  */
 public class Chapter6_22 {
+	static int[][] board;
+	
 	public static void main(String args[]) {
+		int N = 8;
+		board = new int[N][N];
+		placeNQueens(N, 0);
 		
-		int[][] chessboard = new int[8][8];
-		
-		
-
-}
+	}
 	
+	static boolean placeNQueens(int N, int row) {
+		if (row == N) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(board[i][j] + " ");
+				}
+				System.out.println();
+			}
+			return true;
+		}
+		for(int i = 0; i < N; i++) {
+			if (isPossible(N, row, i)) {
+				board[row][i] = 1;
+				
+				if (placeNQueens(N, row + 1)) {
+					return true;
+				}
+			}
+			board[row][i] = 0;
+		}
+		
+		return false;
+	}
 	
+	static boolean isPossible(int N, int row, int col) {
+		for (int i = 0; i < row; i++) {
+			if (board[i][col] == 1) {
+				return false;
+			}
+		}
+		
+		for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+			if (board[i][j] == 1) {
+				return false;
+			}
+		}
+		
+		for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
+			if (board[i][j] == 1) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
