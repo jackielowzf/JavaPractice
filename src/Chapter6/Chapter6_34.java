982608040754509364234f4b7abee0a64e8e9686
jackielowzf <low_zhenfeng@yahoo.com>
2026-08-28 12:58:00 -0400

    Started Chapter 6.34

---
+++ src/Chapter6/Chapter6_34.java
@@ -0,0 +1,13 @@
+package Chapter6;
+/*
+ * Chapter 6.34
+ * (Game: multiple Eight Queens solutions) Exercise 6.22 finds one solution for the
+Eight Queens problem. Write a program to count all possible solutions for the
+Eight Queens problem and display all solutions
+ */
+public class Chapter6_34 {
+	public static void main(String args[]) {
+		
+	}
+
+}
