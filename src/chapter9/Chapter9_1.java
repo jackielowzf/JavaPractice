bdc476f778dced198f1d2f5c368ab9e9a131e5d6
jackielowzf <low_zhenfeng@yahoo.com>
2026-09-14 16:02:12 -0400

    Chapter 9.1

--- src/chapter9/Chapter9_1.java
+++ src/chapter9/Chapter9_1.java
@@ -1,4 +1,5 @@
 package chapter9;
+import java.util.Scanner;
 /*
    	Chapter 9.1
    	
@@ -9,7 +10,25 @@ Valid SSN for a correct Social Security number and Invalid SSN otherwise
  */
 public class Chapter9_1 {
 	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Enter a Social Security Number (DDD-DD-DDDD): ");
+		String ssn = input.next();
+		
+		isValidNumber(ssn);
+		
+		input.close();
+	}
+	
+	public static boolean isValidNumber(String number) {
+		
+		if (number.matches("\\d{3}-\\d{2}-\\d{4}")) {
+			System.out.println("Valid SSN.");
+		}
+		else
+			System.out.println("Invalid SSN");
+		
+		return false;
 	}
 
 }
