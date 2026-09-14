5792715f7973c8f1689faa2f86d919b36fafafb9
jackielowzf <low_zhenfeng@yahoo.com>
2026-09-14 15:50:35 -0400

    Added comments

--- src/chapter9/Chapter9_1.java
+++ src/chapter9/Chapter9_1.java
@@ -2,7 +2,9 @@ package chapter9;
 /*
    	Chapter 9.1
    	
-   	test
+   	(Check SSN ) Write a program that prompts the user to enter a Social Security
+number in the format DDD-DD-DDDD, where D is a digit. The program displays
+Valid SSN for a correct Social Security number and Invalid SSN otherwise
    	
  */
 public class Chapter9_1 {
