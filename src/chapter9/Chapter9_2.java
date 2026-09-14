bb59c6882ad5de66cc15baecfc5f3a589d50c865
jackielowzf <low_zhenfeng@yahoo.com>
2026-09-14 17:06:17 -0400

    Started Chapter 9.2

---
+++ src/chapter9/Chapter9_2.java
@@ -0,0 +1,19 @@
+package chapter9;
+/*
+   Chapter 9.2
+   (Check substrings) You can check whether a string is a substring of another string
+by using the indexOf method in the String class. Write your own method for
+this function. Write a program that prompts the user to enter two strings, and
+checks whether the first string is a substring of the second
+ 
+ */
+public class Chapter9_2 {
+	public static void main(String args[]) {
+		
+	}
+	
+	public static void isSubstring(String a, String b) {
+		
+	}
+
+}
